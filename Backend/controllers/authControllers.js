8e2535f00f68b2467d33a991d553ba4539a9b384
Sawastik Bhullar <sawastik7@gmail.com>
2026-06-20 17:53:31 +0530

    the auth flow and its queries are working fine, added hashing and now moving toward jwt token generation

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,5 +1,5 @@
 import { pool } from "../config/db.js";
-
+import bcrypt from 'bcrypt';
 
 export const handleRegister=async(req,res)=>{
     const{name,email,password}=req.body;
@@ -12,13 +12,14 @@ export const handleRegister=async(req,res)=>{
     }
  ;
 
+ const hashedPass=await bcrypt.hash(password,10);
 
         try{
 
 const result=await pool.query(`INSERT INTO users (name,email,password)
      VALUES ($1,$2,$3)
      RETURNING name,email;
-     `,[name,email,password]);
+     `,[name,email,hashedPass]);
 
 console.log('Data Inserted successfylly');
 
@@ -48,13 +49,38 @@ export const handleLogin=async(req,res)=>{
             message:"Invalid request! all fields are mandatory"
         });
     }
+    try{
 
-        try{
+const result=await pool.query(`
+    SELECT * FROM users 
+    WHERE email=$1
+    ;
+    `,[email]);
+
+    if(result.rows.length ==0){
+        return res.status(404).json({
+            message:"User not found"
+        });
+    };
 
 
 
+    const resultPass=result.rows[0].password;
 
+    const comparedPass=await bcrypt.compare(password,resultPass);
 
+    if(!comparedPass){
+         return res.status(400).json({
+            message:"Bad request"
+        })
+    }
+
+    return res.status(200).json({
+        message:"Login was successful , yayyyyyy!"
+    })
+
+    
+    
         }catch(error){
             return res.status(500).json({
                 message:"Internal server error"
