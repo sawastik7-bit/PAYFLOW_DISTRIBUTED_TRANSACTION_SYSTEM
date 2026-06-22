4681f1a60155f4c4a9925a5989e54647a5d8e83c
Sawastik Bhullar <sawastik7@gmail.com>
2026-06-22 11:29:12 +0530

    added an auth middlewre for other routes

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+import dotenv from 'dotenv';
+dotenv.config();
+export const authMiddleware=async(req,res,next)=>{
+    const token=req.cookies.token;
+
+    if(!token){
+       return res.status(401).json({
+            message:"Unauthorized access"
+        });
+    }
+        try{
+const result=  jwt.verify(token,process.env.JWT_SECRET);
+
+
+req.user=result;
+
+    console.log("auth successfull");
+
+next();
+
+
+
+
+
+        }catch(error){
+            console.log(error);
+            return res.status(401).json({
+    success: false,
+    message: "Unauthorized access"
+});
+        }
+    
+}
