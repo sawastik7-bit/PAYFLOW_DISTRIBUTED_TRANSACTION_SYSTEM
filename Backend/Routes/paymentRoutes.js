31198ca3394423a23de19718d9f62dc9e10f5ec2
Sawastik Bhullar <sawastik7@gmail.com>
2026-06-23 21:15:21 +0530

    added a function to verify the signature in the backend before initiating the database transaction

--- Backend/Routes/paymentRoutes.js
+++ Backend/Routes/paymentRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import razorpayInstance from '../Instances/razorpay.js';
 import { authMiddleware } from '../middleware/authMiddleware';
-import { handleOrderCreate } from '../controllers/paymentController.js';
+import { handleOrderCreate,handleVerificationOfPayment } from '../controllers/paymentController.js';
 const paymentRouter=express.Router();
 
 
 paymentRouter.post("/create-order",authMiddleware,handleOrderCreate);
+paymentRouter.post("/verify-payment",authMiddleware,handleVerificationOfPayment);
 
 export default paymentRouter;
--- Backend/controllers/paymentController.js
+++ Backend/controllers/paymentController.js
@@ -1,5 +1,5 @@
 import razorpayInstance from "../Instances/razorpay";
-
+import crypto from 'crypto';
 
 export const handleOrderCreate=async(req,res)=>{
 
@@ -19,4 +19,56 @@ export const handleOrderCreate=async(req,res)=>{
         res.status(500).send('Error creating razorpay order');
     }
 
+}
+
+
+export const handleVerificationOfPayment=async(req,res)=>{
+
+    const {razorpay_payment_id,
+        razorpay_order_id,
+        razorpay_signature}=req.body;
+
+        const generatedSignature=createdSignature(razorpay_payment_id,razorpay_order_id);
+
+        
+        try{
+
+if(generatedSignature!==razorpay_signature){
+    return res.status(400).json({
+        message:"Unauthorized transaction",
+        success:false
+    });
+
+
+}
+
+
+        }catch(error){
+            return res.status(500).send("Internal server Error");
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const createdSignature=(razorpay_payment_id,razorpay_order_id)=>{
+const hmac=crypto.createHmac('sha256',process.env.RAZORPAY_KEY_SECRET);
+
+        hmac.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+       const digestResult= hmac.digest('hex');
+ return digestResult;
 }
